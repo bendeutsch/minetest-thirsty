fbc1d62c5fea5151273b22b2c9e8143f6684e1fa
Ben Deutsch <ben@bendeutsch.de>
2015-11-01 21:14:16 +0100

    Slight bugfix to PPA

--- persistent_player_attributes.lua
+++ persistent_player_attributes.lua
@@ -104,6 +104,8 @@ PPA.set_value = function(player, name, value)
     local def = PPA.defs[name]
     local inv = player:get_inventory()
     local player_name = player:get_player_name()
+    if value > def.max then value = def.max end
+    if value < def.min then value = def.min end
     PPA.read_cache[player_name][name] = value
     inv:set_stack(name, 1, ItemStack({ name = ":", count = _count_for_val(value, def) }))
 end
